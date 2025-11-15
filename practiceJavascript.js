3c9ea84138f4f832c19961356c0f8f5b2d4e4e40
Varunreddy1328 <kesireddyvarunreddy08@gmail.com>
2025-11-15 16:13:13 +0530

    I have Added Multiplication and Addition Functions

--- practiceJavascript.js
+++ practiceJavascript.js
@@ -1,1 +1,11 @@
 console.log("Hello, World!");
+
+function add(a, b) {
+    return a + b;
+}
+
+function mulitply(a,b){
+    return a*b;
+}
+
+console.log("Multiplication of a and b is: " + mulitply(5, 10));
